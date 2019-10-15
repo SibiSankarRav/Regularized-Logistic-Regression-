c68d6b60c504a56ffeae53db2840c70ab94094d9
Rookie_Aju <56363789+SibiSankarRav@users.noreply.github.com>
2019-10-15 21:04:40 +0200

    Add files via upload
    The hyothesis for the Logistic regression is the sigmoid function.   In order to optimize the parameters, instead of gradient descent steps, the inbuilt function of MATLAB fminunc is used to find the minimum of an unconstrained function.  The parameters of the cost function are being optimized.  The complete regression is evaluated using two datasets

---
+++ costFunctionReg.m
@@ -0,0 +1,42 @@
+function [J, grad] = costFunctionReg(theta, X, y, lambda)
+%COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
+%   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+
+hypo=X*theta;
+%Since it is logistic regression, the hypothesis function changes
+g_hypo=sigmoid(hypo);
+
+%J=((1/m)*sum((-y.*log10(g_hypo))-((1-y).*log10(1-g_hypo))))+((lambda/2*m)*sum((theta_reg).^2));
+J=((1/m)*((-y'*log(g_hypo))-((1-y)'*log(1-g_hypo))))+((lambda/(2*m))*sum((theta(2:end,:).^2)));
+disp(J)
+%J3=(lambda/(2*m))*sum((theta(2:end,:).^2));
+%disp(J3)
+
+
+for i=1:size(theta,1)
+    if (i==1)
+        grad(i)= ((1/m)*sum((g_hypo-y).*X(:,i)));
+    else
+        grad(i)= ((1/m)*sum((g_hypo-y).*X(:,i)))+((lambda/m)*theta(i));
+    end
+end
+
+
+% =============================================================
+
+end
